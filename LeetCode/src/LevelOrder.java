831fad14c1e4e005376574c7c716457ce65214f5
chao.feng <fc2002002002@hotmail.com>
2014-07-03 14:53:35 +0800

    add Populating Next Right Pointers in Each Node in LevelOrder.java

--- LeetCode/src/LevelOrder.java
+++ LeetCode/src/LevelOrder.java
@@ -12,9 +12,34 @@ public class LevelOrder {
 		int[] a={1,2};
 		TreeNode t=CreteBinTree.levelcreate(a);
 		LevelOrder l=new LevelOrder();
-		List<List<Integer>> ll=l.levelOrderBottom(t);
+		List<List<Integer>> ll=l.levelOrder(t);
+		List<List<Integer>> ll2=l.levelOrderBottom(t);
 		System.out.println(ll);
+		System.out.println(ll2);
+
 	}
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ll=new  ArrayList<List<Integer>>();
+        if(root==null)return ll;
+
+    	Queue<TreeNode> q1=new LinkedList<TreeNode>();
+    	Queue<TreeNode> q2=new LinkedList<TreeNode>();
+        q1.offer(root);
+        while(!q1.isEmpty()){
+        	while(!q1.isEmpty())q2.offer(q1.poll());
+        	List<Integer> l=new ArrayList<Integer>();
+        	TreeNode tmp;
+        	while(!q2.isEmpty()){
+        		tmp=q2.poll();
+        		if(tmp.left!=null)q1.offer(tmp.left);
+        		if(tmp.right!=null)q1.offer(tmp.right);
+        		l.add(tmp.val);
+        	}
+        	ll.add(l);
+        }
+      //   Collections.reverse(ll);
+    	return ll;
+    }
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> ll=new  ArrayList<List<Integer>>();
         if(root==null)return ll;
@@ -34,7 +59,9 @@ public class LevelOrder {
         	}
         	ll.add(l);
         }
-         Collections.reverse(ll);
+        Collections.reverse(ll);
     	return ll;
     }
+    
+    
 }
